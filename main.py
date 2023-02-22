993dcf8b4c3acf3514b1cda0052dc94dd5afe898
rubykim-sibel <ruby.kim@sibelinternational.com>
2023-02-22 11:00:53 +0900

    feat: add version

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import json
 from dotenv import load_dotenv
 
 
-def generate_data():
+def generate_data(ver):
     sibel_data = {
         "EnvName": os.getenv("EnvName"),
         "ApiEndpoint": os.getenv("ApiEndpoint"),
         "UserpoolId": os.getenv("UserpoolId"),
         "ClientId": os.getenv("ClientId"),
+        "Version": ver + " " + os.getenv("EnvName").capitalize()
     }
+    print(sibel_data)
     return json.dumps(sibel_data)
 
 
@@ -18,6 +20,7 @@ if __name__ == '__main__':
     # List of .env files to load
     # envs = ['demo', 'dev', 'internal', 'prod', 'rph', 'test']
     envs = ['demo']
+    version = "v1.0"
 
     # Loop through the files and load the environment variables
     for env in envs:
@@ -31,11 +34,11 @@ if __name__ == '__main__':
         )
 
         # Add the JSON data to the QR code
-        qr.add_data(generate_data())
+        qr.add_data(generate_data(version))
 
         # Generate the QR code image
         qr.make(fit=True)
         img = qr.make_image(fill_color="black", back_color="white")
 
         # Save the image to a file
-        img.save(f"{env}.png")
+        img.save(f"images/{env}.png")
